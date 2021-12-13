438f7adab2b1a0e97112023ab84c20430e9fc3ac
Condoriano <15719920+jd4883@users.noreply.github.com>
2021-12-13 05:12:37 +0000

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -1,16 +1,19 @@
 resource "vsphere_datacenter" "dc" { name = var.datacenter_name }
 
 resource "vsphere_compute_cluster" "clusters" {
-  for_each                    = var.clusters
-  datacenter_id               = vsphere_datacenter.dc.moid
-  drs_automation_level        = lookup(each.value, "drs_automation_level", "fullyAutomated")
-  drs_enabled                 = tobool(lookup(each.value, "drs_enabled", true))
-  ha_admission_control_policy = lookup(each.value, "ha_admission_control_policy", "disabled")
-  ha_enabled                  = tobool(lookup(each.value, "ha_enabled", true))
-  ha_vm_component_protection  = lookup(each.value, "ha_vm_component_protection", "disabled")
-  ha_advanced_options         = { "das.ignoreRedundantNetWarning" = "true" }
-  name                        = each.key
-  lifecycle { ignore_changes = [host_system_ids] }
+  for_each                      = var.clusters
+  datacenter_id                 = vsphere_datacenter.dc.moid
+  drs_automation_level          = lookup(each.value, "drs_automation_level", "fullyAutomated")
+  drs_enabled                   = tobool(lookup(each.value, "drs_enabled", true))
+  ha_admission_control_policy   = lookup(each.value, "ha_admission_control_policy", "disabled")
+  ha_enabled                    = tobool(lookup(each.value, "ha_enabled", true))
+  ha_heartbeat_datastore_policy = lookup(each.value, "ha_heartbeat_datastore_policy", "allFeasibleDs")
+  ha_host_monitoring            = lookup(each.value, "ha_host_monitoring", "disabled")
+  ha_vm_restart_priority        = lookup(each.value, "ha_vm_restart_priority", "disabled")
+  ha_vm_component_protection    = lookup(each.value, "ha_vm_component_protection", "disabled")
+  ha_advanced_options           = { "das.ignoreRedundantNetWarning" = "true" }
+  name                          = each.key
+  lifecycle { ignore_changes = [host_system_ids,ha_vm_restart_priority] }
   # TODO: there is a lot you can do with this resource and my use case is simple; this is worth expanding for feature parity
 }
 
